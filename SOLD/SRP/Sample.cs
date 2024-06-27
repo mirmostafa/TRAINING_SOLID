6d024099a9d4805aae181c8e1deaedd5ce591731
Mohammad Mirmostafa <mirmostafa@hotmail.com>
2024-06-27 21:27:37 +0330

    Worked on SRP

---
+++ SOLD/SRP/Sample.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLD.SRP;
+// کلاس ناقض SRP
+public class User
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public void Save()
+    {
+        // ذخیره در پایگاه داده
+    }
+    public bool Validate()
+    {
+        // اعتبارسنجی کاربر
+    }
+}
+
+// اصلاح شده بر اساس SRP
+public class User
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
+
+public class UserRepository
+{
+    public void Save(User user)
+    {
+        // ذخیره در پایگاه داده
+    }
+}
+
+public class UserValidator
+{
+    public bool Validate(User user)
+    {
+        // اعتبارسنجی کاربر
+    }
+}
---
+++ SOLID/SRP/Sample.cs
@@ -0,0 +1,48 @@
+﻿namespace SOLID.SRP
+{
+    namespace Invalid
+    {
+        // کلاس ناقض SRP
+        public class User
+        {
+            public string Password { get; set; }
+            public string Username { get; set; }
+
+            public void Save()
+            {
+                
+            }
+
+            public bool Validate()
+            {
+                return default;
+            }
+        }
+    }
+
+    namespace Valid
+    {
+        // اصلاح شده بر اساس SRP
+        public class User
+        {
+            public required string Password { get; set; }
+            public required string Username { get; set; }
+        }
+
+        public class UserRepository
+        {
+            public void Save(User user)
+            {
+                
+            }
+        }
+
+        public class UserValidator
+        {
+            public bool Validate(User user)
+            {
+                return default;
+            }
+        }
+    }
+}
