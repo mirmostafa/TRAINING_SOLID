237a2f374506817bd367cc0ce5a75b7f89cfe5e2
mirmostafa <mirmostafa@hotmail.com>
2024-07-02 12:48:35 +0330

    Added OCP

---
+++ SOLID/2- OCP/OCP01-Close.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SOLID.OCP.Valid;
+
+namespace SOLID.OCP
+{
+    public class OfflinePayment : IPaymentMethod
+    {
+        public void ProcessPayment()
+        {
+            // تغییر الگوریتم، بدون درگیری با کلاسهای مشابه
+        }
+    }
+
+}
---
+++ SOLID/2- OCP/OCP03-Strategy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.OCP;
+
+public interface IPaymentStrategy
+{
+    void Pay(decimal amount);
+}
+
+public class PaymentContext
+{
+    private IPaymentStrategy _paymentStrategy;
+
+    public PaymentContext(IPaymentStrategy paymentStrategy)
+    {
+        _paymentStrategy = paymentStrategy;
+    }
+
+    public void ExecutePayment(decimal amount)
+    {
+        _paymentStrategy.Pay(amount);
+    }
+}
