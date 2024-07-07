5766ad179784a3bc8b9b1e43e62ad01ad1799c41
mirmostafa <mirmostafa@hotmail.com>
2024-07-07 14:30:14 +0330

    ISP is done.

---
+++ SOLID/4- ISP/ISP02.cs
@@ -0,0 +1,82 @@
+﻿namespace SOLID.ISP02
+{
+    namespace Invalid
+    {
+        public interface IEmployee
+        {
+            void AttendMeeting();
+            void GetSalary();
+            void SubmitTimesheet();
+            void Work();
+        }
+
+        public class Manager : IEmployee
+        {
+            public void Work() { /* Implementation */ }
+            public void AttendMeeting() { /* Implementation */ }
+            public void GetSalary() { /* Implementation */ }
+            public void SubmitTimesheet() { /* Implementation */ }
+        }
+
+        public class RegularEmployee : IEmployee
+        {
+            public void Work() { /* Implementation */ }
+            public void AttendMeeting() { /* Implementation */ }
+            public void GetSalary() { /* Implementation */ }
+            public void SubmitTimesheet() { /* Implementation */ }
+        }
+
+        public class Contractor : IEmployee
+        {
+            public void Work() { /* Implementation */ }
+            public void AttendMeeting() { /* Implementation */ }
+            public void GetSalary() { /* Implementation */ }
+            public void SubmitTimesheet() { /* Implementation */ }
+        }
+        // در اینجا یک مشکل وجود دارد. پیمانکاران نیاز ندارند که به جلسات شرکت کنند یا حقوق بگیرند. با این حال، مجبور هستند این متدها را پیاده‌سازی کنند. این تخطی از اصل جداسازی رابط است.
+    }
+
+    namespace Valid
+    {
+        public interface IWorker
+        {
+            void Work();
+        }
+
+        public interface IMeetingAttendee
+        {
+            void AttendMeeting();
+        }
+
+        public interface ISalariedEmployee
+        {
+            void GetSalary();
+        }
+
+        public interface ITimesheetSubmittable
+        {
+            void SubmitTimesheet();
+        }
+        public class Manager : IWorker, IMeetingAttendee, ISalariedEmployee
+        {
+            public void Work() { /* Implementation */ }
+            public void AttendMeeting() { /* Implementation */ }
+            public void GetSalary() { /* Implementation */ }
+        }
+
+        public class RegularEmployee : IWorker, IMeetingAttendee, ISalariedEmployee, ITimesheetSubmittable
+        {
+            public void Work() { /* Implementation */ }
+            public void AttendMeeting() { /* Implementation */ }
+            public void GetSalary() { /* Implementation */ }
+            public void SubmitTimesheet() { /* Implementation */ }
+        }
+
+        public class Contractor : IWorker, ITimesheetSubmittable
+        {
+            public void Work() { /* Implementation */ }
+            public void SubmitTimesheet() { /* Implementation */ }
+        }
+
+    }
+}
